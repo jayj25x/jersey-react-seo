506dea382f5c01a00a2b377f764d27cb5969bef3
jayj25x <46978727+jayj25x@users.noreply.github.com>
2019-06-21 15:31:33 -0400

    Dice Middleware Configurations
    Main middleware configurations file.

---
+++ src/config/configs.js
@@ -0,0 +1,14 @@
+export const API_KEY = '857a1e5d-e35e-4fdf-805b-a87b6f8364bf';
+export const REALM = 'dce.ufc';
+export const BASE_URL = 'https://dce-frontoffice-stag.imggaming.com/api/v2/';
+export const GUEST_LOGIN_URL = BASE_URL + 'login/guest/checkin';
+export const LOGIN_URL = BASE_URL + 'login';
+export const CONTENT_BASE_URL = BASE_URL + 'content/';
+export const MENU_URL = BASE_URL + 'menu-items';
+export const VOD_BASE_URL = BASE_URL + 'vod/';
+export const EVENT_BASE_URL = BASE_URL + 'event/';
+export const LIVE_EVENTS_URL = BASE_URL + 'event/live';
+export const FAVORITES_URL = BASE_URL + 'favourite/vods';
+export const PLAYLIST_BASE_URL = BASE_URL + 'vod/playlist/';
+export const WATCHED_URL = BASE_URL + 'customer/history/vod';
+export const PREFERENCES_URL = BASE_URL + 'user/preferences';
